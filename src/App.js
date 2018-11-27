6d17611509bd108f81f18706e707c92ad7258ff5
Lucio Flavio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2018-11-27 13:45:07 -0300

    changed version

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SimpleAppBar title="React PWA To-Do App" />
+        <SimpleAppBar title="React PWA To-Do App v1.0" />
         <FilterPanel />
         <VisibleTodoList />
         <TodoFormDialogContainer />
